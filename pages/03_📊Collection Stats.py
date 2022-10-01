6e42a904554e793bae258c8e38b3fd55c42c9ee3
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2022-10-02 01:28:25 +0530

    Update 03_📊Collection Stats.py

--- pages/03_📊Collection Stats.py
+++ pages/03_📊Collection Stats.py
@@ -19,8 +19,8 @@ st.set_page_config(
 st.title("📊 Collection Stats")
 
 # Sidebar Mint Info
-st.sidebar.info("Pirate Life Batch Two: [Mint Now](https://app.hedgey.finance/#/nfts/)")
-# st.sidebar.info("Pirate Life Batch Two: Minted Out!")
+st.sidebar.info("Pirate Life Batch Three: [Mint Now](https://app.hedgey.finance/#/nfts/)")
+# st.sidebar.info("Pirate Life Batch Three: Minted Out!")
 
 # Params
 params_path = "params.yaml"
