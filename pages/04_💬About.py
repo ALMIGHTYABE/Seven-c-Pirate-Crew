845b43871562856c4e964fb4c32d075b00b6d060
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2022-10-02 01:28:37 +0530

    Update 04_💬About.py

--- pages/04_💬About.py
+++ pages/04_💬About.py
@@ -12,8 +12,8 @@ st.set_page_config(
 st.title("💬 About")
 
 # Sidebar Mint Info
-st.sidebar.info("Pirate Life Batch Two: [Mint Now](https://app.hedgey.finance/#/nfts/)")
-# st.sidebar.info("Pirate Life Batch Two: Minted Out!")
+st.sidebar.info("Pirate Life Batch Three: [Mint Now](https://app.hedgey.finance/#/nfts/)")
+# st.sidebar.info("Pirate Life Batch Three: Minted Out!")
 
 # Content
 st.write("""## What is Seven c Pirate Crew?
