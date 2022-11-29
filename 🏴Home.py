014e5a3f20b5070488f7db73568e97a77a3bb5ff
ALMIGHTYABE <107805663+ALMIGHTYABE@users.noreply.github.com>
2022-11-29 22:54:45 +0530

    Update 🏴Home.py

--- 🏴Home.py
+++ 🏴Home.py
@@ -178,6 +178,19 @@ if selection == "Pixel Pirates":
             # Image
             st.image(df["image"].tolist(), caption=["# " + str(i) for i in df["number"]], width=150)  # Images
 
+            # Batch Wise Info
+            batch_count = st.checkbox('See Batchwise Count')
+            if batch_count:
+                col1, col2, col3, col4, col5, col6 = st.columns(6)
+                col1.metric("Batch 1", df[df["Batch"] == 1].shape[0])
+                col2.metric("Batch 2", df[df["Batch"] == 2].shape[0])
+                col3.metric("Batch 3", df[df["Batch"] == 3].shape[0])
+                col4.metric("Batch 4", df[df["Batch"] == 4].shape[0])
+                col5.metric("Batch 5", df[df["Batch"] == 5].shape[0])
+                col6.metric("Batch 6", df[df["Batch"] == 6].shape[0])
+            else:
+                pass
+            
             # Filtered Table
             st.markdown("### Pixel Pirate Data")
             selection = aggrid_interactive_table(
@@ -371,6 +384,20 @@ if selection == "Pirate Life":
                 missing_df = df[df["Type"].isin(missing_type_filter)]
                 missing_nft_df = nft_df[nft_df["Type"].isin(missing_type_filter)]
 
+            # Batch Wise Info
+            batch_count = st.checkbox('See Batchwise Count')
+            if batch_count:
+                col1, col2, col3, col4, col5 = st.columns(5)
+                col1.metric("Treasure", df[df["Batch"] == "Treasure"].shape[0])
+                col2.metric("Batch 1", df[df["Batch"] == "1"].shape[0])
+                col3.metric("Batch 2", df[df["Batch"] == "2"].shape[0])
+                col4.metric("Batch 3", df[df["Batch"] == "3"].shape[0])
+                col5.metric("Batch 4", df[df["Batch"] == "4"].shape[0])
+                # col5.metric("Batch 5", df[df["Batch"] == 5].shape[0])
+                # col6.metric("Batch 6", df[df["Batch"] == 6].shape[0])
+            else:
+                pass
+                
                 # Finding Missing Traits
                 background_missing = [i for i in pd.unique(missing_nft_df["Background"]) if
                                       i not in pd.unique(missing_df["Background"])]
